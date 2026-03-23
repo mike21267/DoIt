d0330ba698b957bcdd82464ab7ea8d1032844b05
mike21267 <mikefem205@gmail.com>
2026-03-23 05:21:24 +0100

    hmm

--- sw.js
+++ sw.js
@@ -1,11 +1,29 @@
 const CACHE = 'doit-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+];
 
 self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
 });
 
 self.addEventListener('push', e => {
@@ -13,8 +31,6 @@ self.addEventListener('push', e => {
   e.waitUntil(
     self.registration.showNotification(data.title, {
       body: data.body,
-      icon: './icon.png',
-      badge: './icon.png',
       vibrate: [200, 100, 200],
       tag: data.tag || 'doit',
     })
@@ -23,7 +39,5 @@ self.addEventListener('push', e => {
 
 self.addEventListener('notificationclick', e => {
   e.notification.close();
-  e.waitUntil(
-    clients.openWindow('/')
-  );
+  e.waitUntil(clients.openWindow('/'));
 });
